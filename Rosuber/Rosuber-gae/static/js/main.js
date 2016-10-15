87b3a63ec7ebafd7d6707174755bc91050e80bdf
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2016-10-14 21:15:05 -0400

    refactor function in main.js

--- Rosuber/Rosuber-gae/static/js/main.js
+++ Rosuber/Rosuber-gae/static/js/main.js
@@ -1,6 +1,19 @@
-$(document).ready(function() {
-       
-   
+var rh = rh || {};
+rh.rb = rh.rb || {};
+
+rh.rb.mdlInitializations = function() {
+  // Polyfill for browsers that don't support the dialog tag.
+  var dialogs = document.querySelectorAll('dialog');
+  for (var i = 0; i < dialogs.length; i++) {
+    // Using an old school style for loop since this for compatibility.
+    var dialog = dialogs[i];
+    if (!dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+  }
+};
+
+rh.rb.enableButtons = function() { 
    
    $("#login-btn").click(function() {
     const registryToken = "de1ce252-8d67-48ba-b6c4-18fa74eeb67a";
@@ -17,4 +30,12 @@ $(document).ready(function() {
     var logoutUrl = $(this).find(".logout-url").html();
     window.location.replace(logoutUrl);
   });
+  
+  
+};
+
+
+$(document).ready(function() {
+  rh.rb.mdlInitializations();
+  rh.rb.enableButtons();
 });
